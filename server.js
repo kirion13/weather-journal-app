a94b7041e62391118d6964f26276d47cdfd44927
kirion13 <kirioncore@gmail.com>
2022-06-12 16:03:01 +0200

    6/12

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 5001;
+
+const baseEndpoint = [];
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+app.use(express.static("website"));
+app.post("/api", (req, res) => {
+  try {
+    const {
+      main: { temp, temp_min, temp_max },
+      weather: [{ main, description }],
+      feeling,
+    } = req.body;
+    baseEndpoint.unshift({
+      temp,
+      temp_min,
+      temp_max,
+      weather: main,
+      description,
+      date: new Date(),
+      feeling,
+    });
+    /////////
+    res.status(201).json(baseEndpoint);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
+app.get("/api", (req, res) => {
+  res.status(200).json(baseEndpoint);
+});
+///
+app.listen(port, () => {
+  console.log(`SERVER IS UP AND RUNNING ON PORT: ${port}`);
+});
---
+++ website/app.js
@@ -0,0 +1,55 @@
+const form = document.querySelector("form");
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
+const API_KEY = "fea6886fedf5c92f8bc46113d33a958e";
+const dataHolder = document.getElementById("Data-holder");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  try {
+    const formData = Object.fromEntries([...new FormData(e.currentTarget)]);
+    console.log(formData);
+    const response = await fetch(
+      `${baseUrl}zip=${formData.zip},us&appid=${API_KEY}&units=metric`
+    );
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
+    data.feeling = formData.feeling;
+    console.log(data.feeling);
+    const serverResponse = await fetch("/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    console.log(serverResponse);
+    const serverData = await serverResponse.json();
+    if (!serverResponse.ok) throw new Error(serverData.message);
+    createUi(serverData);
+  } catch (error) {
+    console.log(error);
+    alert(error.message);
+  }
+});
+const createUi = function (data) {
+  const markup = data.map(
+    (item) =>
+      `
+  <div id="entryHolder">
+  <div>Temprature: ${item.temp} °C</div>
+  <div>Min-Temp: ${item.temp_min} °C</div>
+  <div>Max-Temp ${item.temp_max} °C</div>
+  <div>Weather: ${item.weather}</div>
+  <div>Description: ${item.description}</div>
+  <div>Feeling: ${item.feeling}</div>
+  <div>Date: ${item.date}</div>
+  </div>
+  `
+  );
+  dataHolder.innerHTML = "";
+  dataHolder.insertAdjacentHTML("beforeend", markup);
+};
